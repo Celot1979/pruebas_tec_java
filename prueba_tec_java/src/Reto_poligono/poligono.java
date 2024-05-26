e8f5fb3ec769153fbfba1f54e928ede9d1567f6e
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2024-05-26 12:42:10 +0200

    Creación ejercicio

---
+++ prueba_tec_java/src/Reto_poligono/poligono.java
@@ -0,0 +1,21 @@
+package Reto_poligono;
+/*
+ * Crea una única función (importante que sólo sea una) que sea capaz
+ * de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ */
+public class poligono {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+	private void calculo() {
+		
+	}
+
+}
