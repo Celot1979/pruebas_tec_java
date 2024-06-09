66241dbbc6a293fc2b3c74fe2081d01d8a04cb40
dani <zata_k@hotmail.com>
2024-06-09 12:04:52 +0200

    Comienza tarea de hallar el número menor de una secuencia

---
+++ prueba_tec_java/src/Funcion_Numero_Menor/Numero_Menor.java
@@ -0,0 +1,7 @@
+package prueba_tec_java.src.Funcion_Numero_Menor;
+//Escribe una función que reciba por parámentro uns secuencia de números y que no halle el número menor.
+public class Numero_Menor {
+    public static void main(String[] args) {
+
+    }
+}
