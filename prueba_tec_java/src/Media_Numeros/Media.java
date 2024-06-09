132e505faccdf790ba3ea6de94674a683b9e75f9
dani <zata_k@hotmail.com>
2024-06-09 11:57:30 +0200

    Finalizada tarea de hallar la media de una secuencia

--- prueba_tec_java/src/Media_Numeros/Media.java
+++ prueba_tec_java/src/Media_Numeros/Media.java
@@ -1,23 +1,24 @@
 package prueba_tec_java.src.Media_Numeros;
 /*Escribir un programa que pida al usuario un número indeterminado de enteros y se realice la media del mismo */
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Media {
     public static void main(String[] args) {
         System.out.println("Cuantos números tendrá la secuencia");
         Scanner entrada = new Scanner(System.in);
-        boolean flag = true;
-
-        while (flag) {
-            System.out.print("Introduzca el numero: ");
-            int numero = entrada.nextInt();
-            int suma = 0;
-            int [] secuencial = new int [entrada.nextInt()];
-
-            for (int i = 0; i < secuencial.length; i++) {
-                suma += secuencial[i];
-            }
+        float cantidad_secuencia = entrada.nextInt();
+        //System.out.println(cantidad_secuencia);
+        float suma=0;
+        for (float i =0; i< cantidad_secuencia;i++){
+            System.out.println("Introduce los números que deseas para calcular la media");
+            float secuencia = entrada.nextInt();
+            suma+= secuencia;
         }
+        System.out.println("La suma de la secuencía es : " +  suma);
+        System.out.println("La cantidad de números de la secuencia  es : " +  cantidad_secuencia);
+        System.out.println("La media de la secuencía es : " +  (suma/cantidad_secuencia));
+
 
 
     }
