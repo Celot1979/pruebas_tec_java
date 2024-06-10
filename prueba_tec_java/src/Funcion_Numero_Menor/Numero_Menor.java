8c3aea0fe2a6472bef234fcb5dbc48e82fe3f1e9
dani <zata_k@hotmail.com>
2024-06-10 16:22:50 +0200

    Prueba

--- prueba_tec_java/src/Funcion_Numero_Menor/Numero_Menor.java
+++ prueba_tec_java/src/Funcion_Numero_Menor/Numero_Menor.java
@@ -2,6 +2,7 @@ package prueba_tec_java.src.Funcion_Numero_Menor;
 //Escribe una función que reciba por parámentro uns secuencia de números y que no halle el número menor.
 public class Numero_Menor {
     public static void main(String[] args) {
+        // hola
 
     }
 }
