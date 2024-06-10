0c06bca207c6c0abe4c8e699106b4cc7179c9ab9
dani <zata_k@hotmail.com>
2024-06-10 16:55:29 +0200

    Comienzo de la prueba del Viernes 13

--- prueba_tec_java/src/Funcion_Numero_Menor/Numero_Menor.java
+++ prueba_tec_java/src/Funcion_Numero_Menor/Numero_Menor.java
@@ -1,8 +1,18 @@
 package prueba_tec_java.src.Funcion_Numero_Menor;
+
+import java.util.Arrays;
+
 //Escribe una función que reciba por parámentro uns secuencia de números y que no halle el número menor.
 public class Numero_Menor {
     public static void main(String[] args) {
-        // hola
 
+        int [] secuencia = {30,16,99,6,12,125,20};
+        menor(secuencia);
     }
+
+    private static void menor(int[] secuencia) {
+        Arrays.sort(secuencia);
+        System.out.print("El número menor del arreglo es : " + secuencia[0] +  " Willa Maligana");
+    }
+
 }
