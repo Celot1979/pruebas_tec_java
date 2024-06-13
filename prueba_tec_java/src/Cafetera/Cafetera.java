74440254e698b290636f055b81f86716d85b0465
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2024-06-13 09:48:15 +0200

    1º Paso Cafetera

--- prueba_tec_java/src/Cafetera/Cafetera.java
+++ prueba_tec_java/src/Cafetera/Cafetera.java
@@ -1,7 +1,40 @@
 package prueba_tec_java.src.Cafetera;
 
+import java.util.Scanner;
+
 public class Cafetera {
     public static void main(String[] args) {
-        
-    }
+        Scanner entrada = new Scanner(System.in);
+       // System.out.println("B  I  E  N  V  E  N  I  D  O " + "\n" + "Que bebida desea tomar : ");
+       // System.out.print("M  E  N  Ú " + "\n" + "1. CAFÉ EXPRESSO "+ "\n" +"2. CAFÉ LARGO"+ "\n" +"3. CAFÉ AMERICANO"+ "\n" +"4. CAFÉ CON LECHE"+ "\n" +"5. CEFÉ DESCAFEINADO");
+       // int opcion = entrada.nextInt();
+        boolean flag = false;
+        Controlador eleccion = new Controlador();
+        while (flag == false) {
+            System.out.println("B  I  E  N  V  E  N  I  D  O " + "\n" + "Que bebida desea tomar : ");
+            System.out.print("M  E  N  Ú " + "\n" + "1. CAFÉ EXPRESSO " + "\n" + "2. CAFÉ LARGO" + "\n" + "3. CAFÉ AMERICANO" + "\n" + "4. CAFÉ CON LECHE" + "\n" + "5. CEFÉ DESCAFEINADO" + "\n" + "6. RELLENADO MANUAL"+ "\n" + "7. SALIR" + "\n" + " ===>");
+            opcion = entrada.nextInt();
+            if (opcion == 1) {
+                eleccion.cafe_solo();
+            } else if (opcion == 2) {
+                eleccion.cafe_largo();
+            } else if (opcion == 3) {
+                eleccion.cafe_americano();
+            } else if (opcion == 4) {
+                eleccion.cafe_con_leche();
+            } else if (opcion == 5) {
+                eleccion.cafe_sin_Cafeina();
+            } else if (opcion == 6) {
+                eleccion.rellenado_manual(100,100,100);
+            }else {
+                eleccion.Mensaje();
+                flag=true;
+            }
+
+            }
+        }
+
+
+    private static int opcion;
+
 }
